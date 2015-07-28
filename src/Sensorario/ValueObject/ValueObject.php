b7a96fbd9a3ac390118bded1d222e6a717eef67b
Simone Gentili <sensorario@gmail.com>
2015-07-28 04:25:14 +0000

    Added a complete docblock documentation

--- src/Sensorario/ValueObject/ValueObject.php
+++ src/Sensorario/ValueObject/ValueObject.php
@@ -1,13 +1,25 @@
 <?php
 
+/**
+ * A Value Object implementation in php.
+ */
+
 namespace Sensorario\ValueObject;
 
 use RuntimeException;
 
+/**
+ * A Value Object implementation in php.
+ */
 abstract class ValueObject
 {
     protected $properties = [];
 
+    /**
+     * @param string $functionName function name
+     * @param array  $arguments    arguments passed to that function
+     * {@example test/unit/Sensorario/ValueObject/ValueObjectTest.php 50 61}
+     */
     public function __call($functionName, $arguments)
     {
         return $this->properties[
@@ -15,6 +27,9 @@ abstract class ValueObject
         ];
     }
 
+    /**
+     * @param array $properties all the properties of the Value Object
+     */
     protected function __construct(array $properties)
     {
         $this->properties = $properties;
@@ -23,6 +38,10 @@ abstract class ValueObject
         $this->ensureAllowedProperties();
     }
 
+    /**
+     * @todo create a MissingMandatoryException class
+     * @throws RuntimeException if mandatory parameter is not configured
+     */
     protected function ensureMandatoryProperties()
     {
         foreach ($this->mandatory() as $key) {
@@ -34,6 +53,10 @@ abstract class ValueObject
         }
     }
 
+    /**
+     * @todo create a NotallowedParameterException class
+     * @throws RuntimeException if not allowed parameter is set
+     */
     protected function ensureAllowedProperties()
     {
         $allowed = array_merge(
@@ -50,6 +73,12 @@ abstract class ValueObject
         }
     }
 
+    /**
+     * Generic constructor.
+     * This method aims to generate all Value Objects.
+     *
+     * @return ValueObject new ValueObject instance
+     */
     public static function box(array $properties)
     {
         return new static(
@@ -57,11 +86,23 @@ abstract class ValueObject
         );
     }
 
+    /**
+     * Mandatory properties.
+     * This method returns the array corresponding to the list of all mandatory properties.
+     *
+     * @return array the array of mandatory properties
+     */
     protected static function mandatory()
     {
         return [];
     }
 
+    /**
+     * Allowed properties.
+     * This method returns the array corresponding to the list of all allowed properties.
+     *
+     * @return array the array of allowed properties
+     */
     protected static function allowed()
     {
         return [];
