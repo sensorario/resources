87b67f851fbce4d73412bcdda12973f224aa3005
Simone Gentili <sensorario@gmail.com>
2015-07-31 00:49:11 +0200

    Added docblock for NotAllowedParameterException

--- src/Sensorario/ValueObject/Exceptions/NotAllowedParameterException.php
+++ src/Sensorario/ValueObject/Exceptions/NotAllowedParameterException.php
@@ -1,9 +1,18 @@
 <?php
 
+/**
+ * Not allowed parameter exceptions.
+ * @todo a fix on name typo
+ */
+
 namespace Sensorario\ValueObject\Exceptions;
 
 use Exception;
 
+/**
+ * Not allowed parameter exceptions.
+ * @todo a fix on message?
+ */
 final class NotAllowedParameterEceptions extends Exception
 {
     // ...
