c03438f2bcb35a4027f5c173e01f85b6064046ba
Simone Gentili <sensorario@gmail.com>
2015-10-03 06:10:40 +0200

    Fixed all file summary

--- src/Sensorario/ValueObject/ValueObject.php
+++ src/Sensorario/ValueObject/ValueObject.php
@@ -1,15 +1,14 @@
 <?php
 
 /**
- * Value Object base class.
- * This class aims to be a base class for all kind of Value Objects.
+ * This file is part of sensorario/value-object repository
  *
- * @author Simone Gentili
+ * (c) Simone Gentili <sensorario@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
-/**
- * @author Simone Gentili
- */
 namespace Sensorario\ValueObject;
 
 use RuntimeException;
--- test/unit/Sensorario/ValueObject/ValueObjectTest.php
+++ test/unit/Sensorario/ValueObject/ValueObjectTest.php
@@ -1,9 +1,12 @@
 <?php
 
 /**
- * This is a summary
+ * This file is part of sensorario/value-object repository
  *
- * This is a description
+ * (c) Simone Gentili <sensorario@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Sensorario\ValueObject;
