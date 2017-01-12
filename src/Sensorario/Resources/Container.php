7629fd2fdb3ffa91d8acf700c5daff081c6fc19c
Simone Gentili <sensorario@gmail.com>
2017-01-12 23:50:15 +0100

    push down method

--- src/Sensorario/Resources/Container.php
+++ src/Sensorario/Resources/Container.php
@@ -125,4 +125,44 @@ class Container
     {
         return $this->globals;
     }
+
+    public function mandatory($resource)
+    {
+        return $this->getConstraints(
+            'mandatory',
+            $resource
+        );
+    }
+
+    public function defaults($resource)
+    {
+        return $this->getConstraints(
+            'defaults',
+            $resource
+        );
+    }
+
+    public function rules($resource)
+    {
+        return $this->getConstraints(
+            'rules',
+            $resource
+        );
+    }
+
+    public function allowedValues($resource)
+    {
+        return $this->getConstraints(
+            'allowedValues',
+            $resource
+        );
+    }
+
+    public function ranges($resource)
+    {
+        return $this->getConstraints(
+            'allowedRanges',
+            $resource
+        );
+    }
 }
--- src/Sensorario/Resources/ContainerBase.php
+++ src/Sensorario/Resources/ContainerBase.php
@@ -7,43 +7,7 @@ use \RuntimeException;
 /** @deprecate all these methods will be removed in version 5.0 */
 class ContainerBase
 {
-    public function mandatory($resource)
-    {
-        return $this->getConstraints(
-            'mandatory',
-            $resource
-        );
-    }
-
-    public function defaults($resource)
-    {
-        return $this->getConstraints(
-            'defaults',
-            $resource
-        );
-    }
-
-    public function rules($resource)
-    {
-        return $this->getConstraints(
-            'rules',
-            $resource
-        );
-    }
-
-    public function allowedValues($resource)
-    {
-        return $this->getConstraints(
-            'allowedValues',
-            $resource
-        );
-    }
-
-    public function ranges($resource)
-    {
-        return $this->getConstraints(
-            'allowedRanges',
-            $resource
-        );
-    }
+    /**
+     * @deprecate this class will be removed in 5.0
+     */
 }
